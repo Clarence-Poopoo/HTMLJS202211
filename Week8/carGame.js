6e50a52a30972320c4872cc8d4556e0636dffcf8
Clarence-Poopoo <ctessier@email.neit.edu>
2022-12-07 20:49:51 -0500

    Assignment complete

--- Week8/carGame.js
+++ Week8/carGame.js
@@ -3,14 +3,14 @@ var ctx = canvas.getContext('2d')
 
 var timer = requestAnimationFrame(main)
 
-var start = 50
-var finish = 750
+var start = 58
+var finish = 956
 var carPos = 2
-var speed = 4
+var speed = 3
 
 var startFuel = randomNumber(canvas.width,600)
 var fuel = startFuel
-var fuelBarWidth = 300
+var fuelBarWidth = 512
 var gameOver = true
 var carWidth = 50
 var seconds = 3
@@ -19,7 +19,7 @@ var frames = fps
 
 //load game sprites
 var carSprite = new Image()
-carSprite.src = "images/car.png"
+carSprite.src = "images/car2.png"
 
 carSprite.onload = function(){
     main()
@@ -42,7 +42,7 @@ function main(){
  ctx.clearRect(0,0,canvas.width,canvas.height)
     if(gameOver){
         ctx.fillStyle = "black"
-        ctx.font = "30px Arial"
+        ctx.font = "30px Roboto"
         ctx.textAlign = "center"
         ctx.fillText("Press Space to Start",canvas.width/2,canvas.height/2)
     }else{ 
@@ -74,7 +74,7 @@ function main(){
 }
 
 function drawStartFinish(){
-    ctx.fillStyle = "black"
+    ctx.fillStyle = "gray"
     //start line
     ctx.fillRect(start,50,10,500)
     //finsih line
@@ -91,7 +91,7 @@ function drawFuelBar(){
     var currentBarWidth = fuelBarWidth * (fuel/startFuel)
     ctx.fillStyle = "black"
     ctx.fillRect(start,30,fuelBarWidth,10)
-    ctx.font = "25px Arial"
+    ctx.font = "25px Roboto"
     ctx.fillText("Fuel",start,25)
     if(fuel > 0){
         ctx.fillStyle = "green"
@@ -101,13 +101,13 @@ function drawFuelBar(){
 
 function drawResults(){
     if(carPos + carWidth> finish){
-        ctx.fillStyle = "black"
-        ctx.font = "25px Arial"
+        ctx.fillStyle = "green"
+        ctx.font = "25px Roboto"
         ctx.textAlign = 'center'
         ctx.fillText("You made it to the finish...You Win!", canvas.width/2,canvas.height/2)
     }else{
-        ctx.fillStyle = "black"
-        ctx.font = "25px Arial"
+        ctx.fillStyle = "red"
+        ctx.font = "25px Roboto"
         ctx.textAlign = 'center'
         ctx.fillText("You ran out of fuel...You Lose!", canvas.width/2,canvas.height/2)
     }
@@ -127,7 +127,7 @@ function runStartTimer(){
 
 function drawStartTimer(){
     ctx.fillStyle = "black"
-    ctx.font = "25px Arial"
+    ctx.font = "25px Roboto"
     ctx.textAlign = 'center'
     ctx.fillText(seconds, canvas.width/2,canvas.height/2)
 }
