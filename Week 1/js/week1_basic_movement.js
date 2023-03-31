2c41f1e34ec932b6d51a881415061e70a8f75c08
Clarence-Poopoo <ctessier@email.neit.edu>
2023-03-31 14:18:39 -0400

    Ball

---
+++ Week 1/js/week1_basic_movement.js
@@ -0,0 +1,45 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	player = new Player();
+	//player.vx = -7
+	
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	//Move the Player
+	player.move();
+	
+	//Collision
+	/*if(player.x < player.width/2)
+	{
+		player.x = player.width/2
+		player.vx = -player.vx
+	}
+
+	if(player.x > canvas.width - player.width/2)
+	{
+		player.x = canvas.width - player.width/2
+		player.vx = -player.vx
+	}*/
+
+
+	//Update the Screen
+	player.draw();
+}
