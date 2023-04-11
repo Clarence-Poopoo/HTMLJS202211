30bea339be3d65cee3cf4a7c153f806d98cb90fd
Clarence-Poopoo <ctessier@email.neit.edu>
2023-04-11 13:44:00 -0400

    COUNTER

--- W1/js/week1_basic_movement.js
+++ W1/js/week1_basic_movement.js
@@ -6,6 +6,7 @@ var timer;
 //1000 ms or 1 second / FPS
 var interval = 1000/60;
 var player;
+var score = 0
 
 	//Set Up the Canvas
 	canvas = document.getElementById("canvas");
@@ -13,8 +14,8 @@ var player;
 	
 	//Instantiate the Player
 	player = new Player();
-	player.vx = 7
-	player.vy = 7
+	player.vx = 2
+	player.vy = 2
 	//Set the Animation Timer
 	timer = setInterval(animate, interval);
 
@@ -27,29 +28,37 @@ function animate()
 	player.move();
 	
 	//Collision
+	score = document.getElementById("score").innerHTML = score.toString();;
+
 	if(player.x < player.width/2)
 	{
 		player.x = player.width/2
-		player.vx = -player.vx
+		player.vx = -(player.vx - 2)
+		score ++
+		
 	}
 
 	if(player.x > canvas.width - player.width/2)
 	{
 		player.x = canvas.width - player.width/2
-		player.vx = -player.vx
+		player.vx = -(player.vx + 2)
+		score ++
 	}
 
 	if(player.y < player.width/2)
 	{
 		player.y = player.width/2
-		player.vy = -player.vy
+		player.vy = -(player.vy - 2)
+		score ++
 	}
 
 	if(player.y > canvas.height - player.width/2)
 	{
 		player.y = canvas.height - player.width/2
-		player.vy = -player.vy
+		player.vy = -(player.vy + 2)
+		score ++
 	}
+
 	//Update the Screen
 	player.draw();
 }
